96dcb4132e318d50656eeec957522c4881058152
nb9960 <nbodani8@gmail.com>
2021-08-16 21:48:31 +0530

    modifies ui for shimmer effect

--- lib/components/beacon_card.dart
+++ lib/components/beacon_card.dart
@@ -92,87 +92,85 @@ class BeaconCustomWidgets {
         scrollDirection: Axis.vertical,
         physics: BouncingScrollPhysics(),
         itemCount: 3,
+        padding: const EdgeInsets.all(8.0),
         itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: borderRadius, color: Colors.transparent),
-              child: Container(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: borderRadius,
-                      child: SkeletonAnimation(
-                        child: Container(
-                          width: 110.0,
-                          height: 110.0,
-                          decoration: BoxDecoration(
-                            color: shimmerSkeletonColor,
-                          ),
-                        ),
+          return Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 10.0,
+            ),
+            height: 110,
+            decoration: BoxDecoration(
+              color: kBlue,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.0,
+                  offset: Offset(0.0, 10.0),
+                ),
+              ],
+            ),
+            padding:
+                EdgeInsets.only(left: 16.0, right: 16.0, bottom: 10, top: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15.0, bottom: 10.0, right: 15.0),
+                  child: ClipRRect(
+                    borderRadius: borderRadius,
+                    child: SkeletonAnimation(
+                      child: Container(
+                        height: 15.0,
+                        decoration: BoxDecoration(color: shimmerSkeletonColor),
                       ),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            left: 15.0,
-                            bottom: 15.0,
-                          ),
-                          child: ClipRRect(
-                            borderRadius: borderRadius,
-                            child: SkeletonAnimation(
-                              child: Container(
-                                height: 20.0,
-                                width: MediaQuery.of(context).size.width * 0.5,
-                                decoration:
-                                    BoxDecoration(color: shimmerSkeletonColor),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, right: 5.0, bottom: 15.0),
-                          child: ClipRRect(
-                            borderRadius: borderRadius,
-                            child: SkeletonAnimation(
-                              child: Container(
-                                width: MediaQuery.of(context).size.width * 0.4,
-                                height: 20.0,
-                                decoration:
-                                    BoxDecoration(color: shimmerSkeletonColor),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 15.0, right: 5.0, bottom: 15.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: SkeletonAnimation(
-                              child: Container(
-                                width: MediaQuery.of(context).size.width * 0.3,
-                                height: 20.0,
-                                decoration:
-                                    BoxDecoration(color: shimmerSkeletonColor),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15.0, right: 30.0, bottom: 10.0),
+                  child: ClipRRect(
+                    borderRadius: borderRadius,
+                    child: SkeletonAnimation(
+                      child: Container(
+                        height: 10.0,
+                        decoration: BoxDecoration(color: shimmerSkeletonColor),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15.0, right: 45.0, bottom: 10.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: SkeletonAnimation(
+                      child: Container(
+                        height: 10.0,
+                        decoration: BoxDecoration(color: shimmerSkeletonColor),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0, right: 60.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: SkeletonAnimation(
+                      child: Container(
+                        height: 10.0,
+                        decoration: BoxDecoration(color: shimmerSkeletonColor),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           );
         });
