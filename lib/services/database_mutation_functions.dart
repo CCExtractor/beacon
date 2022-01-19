0c48cbfa0ac679b7b74b439528675765abf0463d
ItsAdityaKSingh <adityakksingh23@gmail.com>
2022-01-19 14:21:04 +0530

    reversed

--- lib/services/database_mutation_functions.dart
+++ lib/services/database_mutation_functions.dart
@@ -29,7 +29,7 @@ class DataBaseMutationFunctions {
   GraphQLError emailAccountPresent =
       const GraphQLError(message: 'Email address already exists');
   GraphQLError wrongCredentials =
-      const GraphQLError(message: 'credentials don\'t match');
+      const GraphQLError(message: 'Invalid credentials');
 
   bool encounteredExceptionOrError(OperationException exception,
       {bool showSnackBar = true}) {
