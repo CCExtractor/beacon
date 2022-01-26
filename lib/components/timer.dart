397bd0ffd4202f3d776cb6ab40cb255e0d384d22
ItsAdityaKSingh <adityakksingh23@gmail.com>
2022-01-26 20:10:37 +0530

    added option to join by clicking snack bar

--- lib/components/timer.dart
+++ lib/components/timer.dart
@@ -1,11 +1,19 @@
 import 'package:beacon/locator.dart';
+import 'package:beacon/models/beacon/beacon.dart';
+import 'package:beacon/utilities/constants.dart';
+import 'package:beacon/views/hike_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_countdown_timer/index.dart';
 
 class CountdownTimerPage extends StatefulWidget {
   final String name;
   final DateTime dateTime;
-  CountdownTimerPage({Key key, @required this.dateTime, @required this.name})
+  final Beacon beacon;
+  CountdownTimerPage(
+      {Key key,
+      @required this.dateTime,
+      @required this.name,
+      @required this.beacon})
       : super(key: key);
   @override
   _CountdownTimerPageState createState() => _CountdownTimerPageState();
@@ -28,8 +36,54 @@ class _CountdownTimerPageState extends State<CountdownTimerPage>
   }
 
   void onEnd() {
-    navigationService
-        .showSnackBar('${widget.name} is now active! You can join the hike');
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 2),
+        content: Text('${widget.name} is now active! You can join the hike'),
+        backgroundColor: Colors.teal,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        behavior: SnackBarBehavior.floating,
+        elevation: 5,
+        action: SnackBarAction(
+          textColor: kYellow,
+          label: 'Click to Join',
+          onPressed: () async {
+            bool isJoinee = false;
+            for (var i in widget.beacon.followers) {
+              if (i.id == userConfig.currentUser.id) {
+                isJoinee = true;
+              }
+            }
+            if ((widget.beacon.leader.id == userConfig.currentUser.id ||
+                isJoinee)) {
+              navigationService.pushScreen(
+                '/hikeScreen',
+                arguments: HikeScreen(
+                  widget.beacon,
+                  isLeader:
+                      (widget.beacon.leader.id == userConfig.currentUser.id),
+                ),
+              );
+            } else {
+              await databaseFunctions.init();
+              final Beacon _beacon =
+                  await databaseFunctions.joinBeacon(widget.beacon.shortcode);
+
+              if (_beacon != null) {
+                navigationService.pushScreen(
+                  '/hikeScreen',
+                  arguments: HikeScreen(widget.beacon, isLeader: false),
+                );
+              }
+            }
+          },
+        ),
+      ),
+    );
   }
 
   @override
