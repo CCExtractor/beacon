b1a829c1e100bc8523b247f3b654ef2247aabdd8
Pavel401 <pavelalam401@gmail.com>
2022-01-26 12:06:10 +0530

    revert to   share_plus: ^3.0.4

--- lib/components/hike_screen_widget.dart
+++ lib/components/hike_screen_widget.dart
@@ -4,7 +4,7 @@ import 'package:beacon/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:sizer/sizer.dart';
 
 class HikeScreenWidget {
