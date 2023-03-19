a2065d8ac0b3afd030ed397d4a200897eadc5305
Firos K <f>
2023-03-19 22:06:28 +0530

    Resolved Error type 'bool' is not a subtype of'Location'

--- lib/models/user/user_info.g.dart
+++ lib/models/user/user_info.g.dart
@@ -16,15 +16,15 @@ class UserAdapter extends TypeAdapter<User> {
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
+    print("fields of 6 bool or not: ${fields[6]}");
     return User(
       authToken: fields[1] as String,
       email: fields[3] as String,
       name: fields[2] as String,
-      location: fields[6] as Location,
+      isGuest: fields[6] as bool ?? false,
       beacon: (fields[4] as List)?.cast<Beacon>(),
       groups: (fields[5] as List)?.cast<Group>(),
       id: fields[0] as String,
-      isGuest: fields[7] as bool,
     );
   }
 
